5c7a22a8f7428fc673e33006751f3902a536db52
Deepu T Philip <deepu.dtp@gmail.com>
2019-11-02 06:49:43 +0530

    Use click file type instead of opening files directly.

--- nbcommands/_less.py
+++ nbcommands/_less.py
@@ -9,11 +9,10 @@ from .terminal import display
 
 @click.command(name="nbless")
 @click.version_option(version=__version__)
-@click.argument("file")
+@click.argument("file", type=click.File('rb'), default='-')
 @click.pass_context
 def less(ctx, *args, **kwargs):
     """View the Jupyter notebook to standard output via a Pager"""
-    with open(kwargs["file"], "r") as f:
-        nb = nbformat.read(f, as_version=4)
-
+    
+    nb = nbformat.read(kwargs['file'], as_version=4)
     click.echo_via_pager("\n".join(display(nb.cells)))
